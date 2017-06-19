de23c4ff2af536f916aa63416814fa22bdda8570
Sachico <g1627059@st.t-kougei.ac.jp>
2017-06-19 17:14:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,4 +4,15 @@ var answer = parseInt(window.prompt('数あてゲーム！0～8の数字を入�
 
 var message;
 if(answer === number) {
+   message = 'あたり！';}
+else if (answer < number){
+   message = '残念...もっと大きいです....';}
+else if (number< answer){
+   message ='残念...もっと小さいです....';}
+else{
+   message ='0～8の数字を入力してください！！';}
+
+
+//結果を表示
+document.getElementById('choice').texyContent = message;
    
